dc4e48ea684efef39d9ba501bb1608f4407f6b84
Axel Vermeil <axel.vermeil@gmail.com>
2018-08-16 12:01:06 +0100

    prettier lint

--- test/test.js
+++ test/test.js
@@ -6,26 +6,24 @@ const bjq = lib.queue
 const messages = lib.error_messages
 
 describe('bjq', () => {
-
-  describe('()', () => {
-    it('should create a new queue instance', () => {
-      const queue = new bjq({
-        mysql_connection : {},
-        job_type: 'test',
-        batch_size: 50
-      })
-      expect(queue).to.be.instanceof(bjq)
-      expect(queue.job_type).to.equal('test')
-      expect(queue.batch_size).to.equal(50)
+    describe('()', () => {
+        it('should create a new queue instance', () => {
+            const queue = new bjq({
+                mysql_connection: {},
+                job_type: 'test',
+                batch_size: 50,
+            })
+            expect(queue).to.be.instanceof(bjq)
+            expect(queue.job_type).to.equal('test')
+            expect(queue.batch_size).to.equal(50)
+        })
+        it('should fail if no job type specified', () => {
+            try {
+                const queue = new bjq({ mysql_connection: {} })
+            } catch (err) {
+                expect(err).to.not.be.null
+                expect(err.message).to.equal(messages['no_job_type'])
+            }
+        })
     })
-    it('should fail if no job type specified', () => {
-      try {
-        const queue = new bjq({ mysql_connection : {}})
-      } catch(err) {
-        expect(err).to.not.be.null
-        expect(err.message).to.equal(messages['no_job_type'])
-      }
-    })
-  })
-
 })
